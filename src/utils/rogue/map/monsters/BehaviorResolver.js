7b784b65879aec66a1f76df0fe3aea0b0b4dd358
Frederic CHAPLIN <frederic.chaplin@gmail.com>
2017-03-28 13:43:07 +0200

    Formalyze detection for monster
    Begin to categorize detections (other monsters, items, players...)

--- src/utils/rogue/map/monsters/BehaviorResolver.js
+++ src/utils/rogue/map/monsters/BehaviorResolver.js
@@ -16,14 +16,49 @@ export default class BehaviorResolver {
 
         //what does the monster see or hear or smell ?
         this.detections = BehaviorResolver.getDetections(monster, map, player);
+        if (this.detections.player.length){
+			console.log(monster.key + ' saw you !!! Run for your life !');
+		}
+		if (this.detections.items.length){
+        	for (let item of this.detections.items){
+        		console.log(monster.key + ' saw ' + item.content.type);
+			}
+		}
+		if (this.detections.occupants.length){
+			for (let occupant of this.detections.occupants){
+				if (this.monster.key !== occupant.occupant)
+					console.log(monster.key + ' saw ' + occupant.occupant);
+			}
+		}
     }
 
     static getDetections(monster, map, player) {
         let detectZone = FieldOfView.getFieldOfView(monster.cell, monster.view, map, false);
+        let detections= {};
+        let items = [];
+        let playerDetection = [];
+        let monsterDetection = [];
         if (detectZone) {
             for (let cell of detectZone) {
                 cell.colored = " red";
+                if (cell.cellContent.length) {
+                    for (let content of cell.cellContent) {
+                        items.push(
+                            { cell, content });
+                    }
+                }
+                if (cell == player) {
+                    playerDetection.push({cell, player});
+                }
+				if (cell.occupant) {
+					let occupant = cell.occupant;
+					monsterDetection.push({cell, occupant});
+				}
             }
         }
+        detections.items = items;
+        detections.player = playerDetection;
+        detections.occupants = monsterDetection;
+        return detections;
     }
 }
